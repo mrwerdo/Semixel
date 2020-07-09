f50ce2a35ac721e00870f9fce91f256a3cae9a94
Andrew Thompson <mrwerdo331@me.com>
2020-07-09 19:02:47 +1000

    Initial Commit

---
+++ Semixel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Semixel
+//
+//  Created by Andrew Thompson on 9/7/20.
+//  Copyright © 2020 Andrew Thompson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
