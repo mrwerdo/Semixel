079b6c881cbaec007cce23b49008d9a0e614f34d
Andrew Thompson <mrwerdo331@me.com>
2020-10-03 11:56:52 +1000

    Made basic views

--- Semixel/ContentView.swift
+++ Semixel/ContentView.swift
@@ -8,9 +8,86 @@
 
 import SwiftUI
 
+struct BigButton: View {
+    
+    var image: String
+    
+    var body: some View {
+        Button(action: {
+            print("button tapped")
+        }) {
+            ZStack(alignment: .center) {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color(.secondarySystemBackground))
+                Image(systemName: image).font(Font.system(size: 36))
+            }
+            .frame(width: 64, height: 64, alignment: .center)
+        }
+    }
+}
+
+struct Tools: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                BigButton(image: "pencil.tip")
+                BigButton(image: "paintbrush")
+                BigButton(image: "circle")
+                BigButton(image: "minus")
+                BigButton(image: "square.on.circle")
+            }
+            HStack {
+                BigButton(image: "selection.pin.in.out")
+                BigButton(image: "magnifyingglass")
+                BigButton(image: "arrow.uturn.left")
+                BigButton(image: "arrow.uturn.right")
+                BigButton(image: "rotate.left")
+            }
+        }
+    }
+}
+
+struct ColorTab: View {
+    var tag: Int
+    var color: Color
+    
+    var body: some View {
+        Button(action: {
+            print("color tab tapped")
+        }) {
+            HStack(spacing: 0) {
+                Rectangle()
+                    .fill(color).frame(width: 32, height: 32, alignment: .center)
+                Image(systemName: "\(abs(tag)).circle")
+                    .background(
+                        Rectangle()
+                            .fill(Color(.secondarySystemBackground)))
+                    .frame(width: 32, height: 32, alignment: .center)
+                    .border(Color(.systemGray3), width: 2.0)
+            }.mask(
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(width: 64, height: 32, alignment: .center))
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .center) {
+            Image("pixel_art")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 365, alignment: .center)
+                .border(Color(.systemGray5), width: 4)
+            Tools()
+            HStack(spacing: 8) {
+                ColorTab(tag: 1, color: Color(.systemBlue))
+                ColorTab(tag: 2, color: Color(.systemGreen))
+                ColorTab(tag: 3, color: Color(.systemIndigo))
+                ColorTab(tag: 4, color: Color(.systemOrange))
+                ColorTab(tag: 5, color: Color(.systemPink))
+            }
+        }
     }
 }
 
